ae3402d7b9c29bdc647cf073ddcf59ff637a98df
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-06-06 16:08:23 +0100

    ex2

--- ex2/routes/index.js
+++ ex2/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+const url = 'https://localhost:17000/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  axios.get(url + 'contratos')
+    .then(dados => {
+      res.render('index', { lista: dados.data , title : 'Lista de Contratos'});
+    })
+    .catch(erro => {
+      res.render('error', { error: erro })
+    });
 });
 
 module.exports = router;
