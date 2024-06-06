225b582dc9c6e1281e79781932bdd3ce9be87967
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-06-06 16:34:31 +0100

    ex2

--- ex2/routes/index.js
+++ ex2/routes/index.js
@@ -1,17 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
-const url = 'https://localhost:17000/';
 
+const api_url = 'http://localhost:17000';
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  axios.get(url + 'contratos')
-    .then(dados => {
-      res.render('index', { lista: dados.data , title : 'Lista de Contratos'});
+  axios.get(api_url + '/books')
+    .then(response => {
+      res.render('index', { lista: response.data, title: 'Lista de Livros' });
     })
-    .catch(erro => {
-      res.render('error', { error: erro })
+    .catch(error => {
+      res.render('error', { error: error });
     });
 });
 
+router.get('/:id', function(req, res, next) {
+  axios.get(api_url + '/books/' + req.params.id)
+    .then(response => {
+      res.render('livro', { Livro: response.data });
+    })
+    .catch(error => {
+      res.render('error', { error: error });
+    });
+});
+
+router.get('/authors/:id', function (req, res) {
+  axios.get(api_url + "/books").then((response) => {
+    let author = req.params.id;
+    let livros = response.data;
+    let livrosAutor = livros.filter(livro => livro.author && livro.author.includes(author));
+    res.render('autor', { autor: author, lista: livrosAutor });
+  }).catch((error) => {
+    res.render('error', { error: error });
+  });
+});
 module.exports = router;
